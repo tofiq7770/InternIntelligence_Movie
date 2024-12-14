cb8992e5b362e535f762f9e8f390656a5beb022d
tofiq7770 <tofigtn@code.edu.az>
2024-12-14 23:56:36 +0400

    Add

---
+++ Movie/DAL/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Movie.Models;
+
+namespace Movie.DAL
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+
+    }
+}
---
+++ Movie/Models/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Movie.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FullName { get; set; }
+    }
+}
